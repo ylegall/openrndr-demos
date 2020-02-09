4f801dab12d0b764883584297ba77771b19b6ede
Yann Le Gall <ylegall@gmail.com>
2020-02-09 13:11:11 -0800

    updates twisting squares demo

--- src/main/kotlin/org/ygl/openrndr/demos/RotatingSquares.kt
+++ src/main/kotlin/org/ygl/openrndr/demos/RotatingSquares.kt
@@ -24,39 +24,31 @@ fun main() = application {
             val color: ColorRGBa
     )
 
-    val bgColor = color(255, 107, 0)
-
     val squares = (1 .. SQUARES).map {
+        val percent = it / SQUARES.toDouble()
+        val color = color(255 * percent, 43 + SQUARES - it, 250 * (1 - percent))
+        val squareWidth = SCREEN_WIDTH * percent
 
-        val color = color(
-                255 * (it/SQUARES.toDouble()),
-                55 + SQUARES - it,
-                250 * (1 - it/SQUARES.toDouble())
-        )
-
-        val squareWidth = (SCREEN_WIDTH) * (it/SQUARES.toDouble())
-        Square(
-                squareWidth,
-                (SCREEN_WIDTH / 2.0 - squareWidth / 2.0),// * (1 - SQUARES.toDouble() / it),
-                color)
+        Square(squareWidth, (SCREEN_WIDTH / 2.0 - squareWidth / 2.0), color)
     }.reversed()
 
+    val bgColor = color(255, 107, 0)
+    val strokeColor = color(16, 4, 222)
     var totalTime = 0.0
 
     program {
 
         extend {
-            drawer.background(bgColor)
-            drawer.stroke = ColorRGBa.BLUE
             totalTime = (SPIN_RATE * deltaTime + totalTime) % 360
-
-            drawer.translate(width/2.0, width/2.0)
+            drawer.background(bgColor)
+            drawer.stroke = strokeColor
+            drawer.translate(width / 2.0, width / 2.0)
 
             squares.forEach {
                 drawer.isolated {
                     fill = it.color
                     rotate(totalTime * it.spinSpeed)
-                    translate(-it.width/2.0, -it.width/2.0)
+                    translate(-it.width / 2.0, -it.width / 2.0)
                     drawer.rectangle(0.0, 0.0, it.width, it.width)
                 }
             }
