6ff5288660b96d2a2b751e50f270cc91993f7d5f
Yann Le Gall <ygall@indeed.com>
2020-02-28 17:10:07 -0800

    adds shell script for making boomerang effect

---
+++ video/boomerang.sh
@@ -0,0 +1,14 @@
+filename=$(basename -- "$1")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+fps=`ffprobe $1 2>&1 | grep -oP "\d+ (?=fps)"`
+echo "fps: $fps"
+duration=`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $1`
+duration_int=${duration%.*}
+echo "duration: $duration_int"
+total_frames=$((duration_int * fps))
+echo "total frames: $total_frames"
+
+ffmpeg -i $1 -filter_complex "[0]reverse[r];[0][r]concat,loop=0.5:$total_frames,setpts=N/$fps/TB" "$filename-boomerang.$extension"
+
